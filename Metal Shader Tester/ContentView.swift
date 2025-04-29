d36695e251040b4f84713b0746f9bfacffd949fe
Charlie Minow <minow@mac.com>
2025-04-29 09:30:45 -0600

    Initial Commit

---
+++ Metal Shader Tester/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metal Shader Tester
+//
+//  Created by Charlie Minow on 4/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metal Shader Tester/Metal_Shader_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_Shader_TesterApp.swift
+//  Metal Shader Tester
+//
+//  Created by Charlie Minow on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Metal_Shader_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
