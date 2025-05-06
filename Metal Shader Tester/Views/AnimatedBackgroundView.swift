58a18bf641d4085fb85215359c94c0fffefae437
Charlie Minow <minow@mac.com>
2025-05-05 19:09:01 -0600

    Just some code clean-up.

--- Metal Shader Tester/Views/AnimatedBackgroundView.swift
+++ Metal Shader Tester/Views/AnimatedBackgroundView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct AnimatedBackgroundView: View {
     @State private var start: Date = Date()
+
     var body: some View {
         TimelineView(.periodic(from: .now, by: 0.050)) { context in
-//            let _ = print(Date.now.timeIntervalSinceReferenceDate)
             let time = start.distance(to: context.date)
             Rectangle()
                 .visualEffect { content, proxy in
