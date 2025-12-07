8e64f35a15dd98980f9d56e48937e13b41fc51e7
Charlie Minow <minow@mac.com>
2025-12-07 16:46:30 -0700

    Polka dots are now just squared out rows.

---
+++ Metal Shader Tester/Metal Shaders/Hexagons.metal
@@ -0,0 +1,11 @@
+//
+//  Hexagons.metal
+//  Metal Shader Tester
+//
+//  Created by Charlie Minow on 11/16/25.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
---
+++ Metal Shader Tester/Views/HexagonsView.swift
@@ -0,0 +1,18 @@
+//
+//  HexagonsView.swift
+//  Metal Shader Tester
+//
+//  Created by Charlie Minow on 11/16/25.
+//
+
+import SwiftUI
+
+struct HexagonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HexagonsView()
+}
