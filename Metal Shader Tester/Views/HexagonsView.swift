90cb7446598b100f9205bd739f26f7ca398f9957
Charlie Minow <minow@mac.com>
2026-09-26 08:09:45 -0600

    Add a switch on the hexagons view so I can see it with an image.

--- Metal Shader Tester/Views/HexagonsView.swift
+++ Metal Shader Tester/Views/HexagonsView.swift
@@ -11,15 +11,32 @@ struct HexagonsView: View {
     @State private var scale: Float = 12.0
     @State private var hexInset: Float = 0.95
     @State private var softness: Float = 2.0
+    @State private var layerSource = "Gradient"
+    let layerSources = ["Gradient", "Image"]
     
     var body: some View {
         VStack {
-            Rectangle()
-                .fill(
-                    LinearGradient(colors: [.black, .gray, .yellow], startPoint: .top, endPoint: .bottom)
-                )
-                .hexPixellateLayer(scale: scale, hexInset: hexInset, softness: softness)
-                .clipped()
+            if layerSource == "Gradient" {
+                Rectangle()
+                    .fill(
+                        LinearGradient(colors: [.black, .gray, .yellow], startPoint: .top, endPoint: .bottom)
+                    )
+                    .hexPixellateLayer(scale: scale, hexInset: hexInset, softness: softness)
+                    .clipped()
+            } else {
+                Image("roseImage")
+                    .hexPixellateLayer(scale: scale, hexInset: hexInset, softness: softness)
+                    .clipped()
+            }
+            
+
+            Picker("Layer Source", selection: $layerSource) {
+                ForEach(layerSources, id: \.self) { layerSource in
+                    Text(layerSource).tag(layerSource)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding()
             
             VStack(alignment: .leading) {
                 Text("Scale: \(scale.formatted(.number.precision(.fractionLength(2))))")
